9b8dad4f6e3e9d4d33b22bf4e169aa9b623c820c
Duncan Stevenson-Price <dst87@users.noreply.github.com>
2017-03-28 10:43:03 +0100

    Initial commit with basic support for pausing and resuming SAB User support such that only specificed users can interact

---
+++ butler.php
@@ -0,0 +1,155 @@
+<?php
+// Import configuraton file and auxilliary code
+include("settings.php");
+
+$SABServer = rtrim($SABServer, "/");
+$SABBaseURL = $SABServer."/api?output=json&apikey=".$SABAPIKey;
+
+
+// Import and setup Telegram API
+include("Telegram.php");
+$telegram = new Telegram($bot_id);
+
+// Take text and chat_id from the message
+$result = $telegram->getData();
+$text = $telegram->Text();
+$chat_id = $telegram->ChatID();
+
+
+file_put_contents("log.log", print_r($result, true));
+
+
+
+if (array_key_exists("message", $result)) {
+  // Check only permitted users are being parsed
+  $user = $result["message"]["from"]["username"];
+  if (!in_array($user, $permittedUsers)) {
+    die();
+  }
+
+  // Check message text and chat ID exists.
+  if(is_null($text) || is_null($chat_id)) {
+    die();
+  }
+
+  normalMessage();
+} else if (array_key_exists("callback_query", $result)) {
+  callbackQuery();
+}
+else{
+  die();
+}
+
+
+function normalMessage(){
+  global $result, $text;
+
+  // Switch on command
+  // NOTE: This will deal only with the first command it finds
+  // in the array of entities
+
+  $entities = $result["message"]["entities"];
+  foreach ($entities as $entity) {
+    if($entity["type"] == "bot_command"){
+      $offset = $entity["offset"];
+      $length = $entity["length"];
+      parseCommand(substr($text,$offset+1,$length-1));
+      break;
+    }
+  }
+}
+
+function callbackQuery(){
+  global $result, $telegram;
+  $callbackID = $result["callback_query"]["id"];
+  $buttonData = $result["callback_query"]["data"];
+  $originalMessage = $result["callback_query"]["message"]["text"];
+  $messageID = $result["callback_query"]["message"]["message_id"];
+  switch ($originalMessage) {
+    case "Very good, sir.  For how long would you like me to pause your downloads?":
+      pauseSAB($buttonData);
+      answerCallbackQuery($callbackID, "Downloads paused.");
+      break;
+
+    default:
+      break;
+  }
+}
+
+
+function parseCommand($cmd){
+  switch ($cmd) {
+    case "pause":
+      pauseSABPrompt();
+      break;
+    case "resume":
+      resumeSAB();
+      break;
+    case "setspeed":
+      setSABSpeed();
+      break;
+    default:
+      $message = "Command not recognised, sorry!";
+      break;
+  }
+}
+
+
+function pauseSABPrompt() {
+  global $SABBaseURL, $telegram;
+
+  $b15 = $telegram->buildInlineKeyboardButton("15 min", "", "15");
+  $b30 = $telegram->buildInlineKeyboardButton("30 min", "", "30");
+  $b60 = $telegram->buildInlineKeyboardButton("60 min", "", "60");
+  $b180 = $telegram->buildInlineKeyboardButton("3 hr", "", "180");
+  $b360 = $telegram->buildInlineKeyboardButton("6 hr", "", "360");
+  $b540 = $telegram->buildInlineKeyboardButton("9 hr", "", "540");
+  $bIND = $telegram->buildInlineKeyboardButton("Indefinitely", "", "indefinitely");
+
+  $kbd = $telegram->buildInlineKeyboard([[$b15, $b30, $b60],[$b180, $b360, $b540],[$bIND]]);
+
+  sendMessageWithInlineKeyboard("Very good, sir.  For how long would you like me to pause your downloads?", $kbd);
+}
+
+function pauseSAB($time) {
+  global $SABBaseURL;
+  if($time == "indefinitely"){
+    $URL = $SABBaseURL."&mode=pause";
+  }
+  else {
+    $URL = $SABBaseURL."&mode=config&name=set_pause&value=".$time;
+  }
+  $result = file_get_contents($URL);
+}
+
+function resumeSAB() {
+  global $SABBaseURL;
+  $URL = $SABBaseURL."&mode=resume";
+  $result = file_get_contents($URL);
+  sendMessage("Your downloads have been resumed, sir.");
+}
+
+function setSABSpeed() {
+
+}
+
+function sendMessage($message){
+  global $chat_id, $telegram;
+  $content = array('chat_id' => $chat_id, 'text' => $message);
+  $telegram->sendMessage($content);
+}
+
+function sendMessageWithInlineKeyboard($message, $keyboard){
+  global $chat_id, $telegram;
+  $content = array('chat_id' => $chat_id, 'text' => $message, 'reply_markup' => $keyboard);
+  $telegram->sendMessage($content);
+}
+
+function answerCallbackQuery($callbackID, $message){
+  global $telegram;
+  $content = array('callback_query_id' => $callbackID, 'text' => $message);
+  $telegram->answerCallbackQuery($content);
+
+}
+
+?>
---
+++ settings.example.php
@@ -0,0 +1,28 @@
+<?php
+
+// **********************
+// TELEGRAM CONFIGURATION
+// **********************
+
+// Telegram bot ID.  Speak to Telegram's BotFather to set up your bot
+$bot_id = "";
+
+// This should be a list of Telegram usernames permitted to interact with the bot
+// Anyone else who attempts to interact with the bot will simply be ignored.
+
+$permittedUsers = ["usera", "userb"];
+
+
+// **********************
+// SABnzbd CONFIGURATION
+// **********************
+
+// Configure SABnzbd.
+// Change the URL to whatever you use, and change the API key
+// to the appropriate one for your own installation.  You can get this
+// in SABnzbd configuration on the web interface.
+
+$SABServer = "http://192.168.1.1";
+$SABAPIKey = "PASTE API KEY HERE";
+
+?>
